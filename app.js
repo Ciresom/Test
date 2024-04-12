199170692e324a95691cda6731849b298fc59e74
Ciresom <166697177+Ciresom@users.noreply.github.com>
2024-04-12 15:16:39 +0200

    card

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const cardArray = [
     {
-        name:'fries',
-        img: 'images/fries.jpg'
-    }
+        name: 'fries',
+        img: 'images/fries.jpg',
+    },
+    {
+        name: 'burger',
+        img: 'images/burger.jpg',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.jpg',
+    },
+    {
+        name: 'smoothie',
+        img: 'images/smoothie.jpg',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.jpg',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.jpg',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.jpg',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.jpg',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.jpg',
+    },
+    {
+        name: 'burger',
+        img: 'images/burger.jpg',
+    },
+    {
+        name: 'smoothie',
+        img: 'images/smoothies.jpg',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.jpg',
+    },
+
 ]
+console.log(cardArray)
